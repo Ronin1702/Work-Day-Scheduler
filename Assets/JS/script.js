fa4b020bac5eaa1c6e614fc1bc5ec42822e25a1a
Ronin1702 <kchen@me.com>
2023-05-25 19:24:33 -0400

    I try different syntext for the comparion

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -23,33 +23,33 @@ $('#currentDay').html(currentDate);
 //   // time-block containing the button that was clicked? How might the id be
 //   // useful when saving the description in local storage?
 //   //
-        //Make a click listener to the saveBtn and get the values stored: 
-        $('.saveBtn').on('click',function(){
-          var hour = $(this).parent().attribute('id');
-          var tasks = $(this).siblings(".description").val();
-          console.log(hour, tasks)
-        // Adding the descriptions in the localStorage:
-          localStorage.setItem(hour, tasks)
-        })
+//Make a click listener to the saveBtn and get the values stored: 
+$('.saveBtn').on('click', function () {
+  var hour = $(this).parent().attribute('id');
+  var tasks = $(this).siblings(".description").val();
+  console.log(hour, tasks)
+  // Adding the descriptions in the localStorage:
+  localStorage.setItem(hour, tasks)
+})
 
-        function hourTracker(){
-          var currentHour = dayjs().hour();
-          
-          $('.time-block').each(function(){
-            var timeBlock =parseInt($(this).attribute('id').split('hour-')[1]);
+function hourTracker() {
+  var currentHour = dayjs().hour();
 
-            if(timeBlock === currentHour) {
-              $(this).setAttribute('present');
-           } else if (timeBlock>currentHour){
-              $(this).setAttribute('future');
-       
-              
-           } else{
-              $(this).setAttribute('past');
+  $('.time-block').each(function () {
+    var timeBlock = parseInt($(this).attribute('id').split('hour-')[1]);
 
-           }
-          })
-      }
+    if (timeBlock === currentHour) {
+      $(this).setAttribute('present');
+    } else if (timeBlock > currentHour) {
+      $(this).setAttribute('future');
+
+
+    } else {
+      $(this).setAttribute('past');
+
+    }
+  })
+}
 //   // TODO: Add code to apply the past, present, or future class to each time
 //   // block by comparing the id to the current hour. HINTS: How can the id
 //   // attribute of each time-block be used to conditionally add or remove the
@@ -61,16 +61,16 @@ $('#currentDay').html(currentDate);
 //   // attribute of each time-block be used to do this?
 
 //   // Retrieve items from the localStorage
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    hourTracker();
+hourTracker();
 // });
 
