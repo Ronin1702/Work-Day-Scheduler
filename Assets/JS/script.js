d318008414016bf024906b5455f44eb6cd0cddc1
Ronin1702 <kchen@me.com>
2023-05-25 21:27:12 -0400

    I changed the class in all hour-XX in div to future for easier manipulation in java

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,26 +7,25 @@
 
 // Below I try the jQuery method
 var currentDate = dayjs().format('dddd, MMMM DD, YYYY');
-console.log(currentDate)
-$('#currentDay').html(currentDate);
+console.log(currentDate);
+$('#currentDay').text(currentDate);
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-
   //Make a click listener to the saveBtn and get the values stored: 
-  $('.saveBtn').on('click', function () {
+  $('.saveBtn').on('click', function () {});
     // define and allocate the hour of the related time-block.
     // this here refers to the same line where .saveBtn class is located.
     var hour = $(this).parent().attr('id');
     // definte inputs in the description box as tasks.
     var tasks = $(this).siblings(".description").val();
     // console log to check if my variables are set up correctly
-    console.log(hour, tasks)
+    console.log(hour, tasks);
     // Storing the descriptions and related time-block in the localStorage:
-    localStorage.setItem(hour, tasks)
-  })
+    localStorage.setItem(hour, tasks);
+  });
 
   function hourTracker() {
 
@@ -34,8 +33,8 @@ $(document).ready(function () {
 
     $('.time-block').each(function () {
       var timeBlock = parseInt($(this).attr('id').split('hour-')[1]);
-      console.log(timeBlock)
-      console.log(currentHour)
+        console.log(timeBlock);
+        console.log(currentHour);
 
       if (timeBlock === currentHour) {
         $(this).removeClass('future');
@@ -56,16 +55,15 @@ $(document).ready(function () {
   }
 
   //   // Retrieve items from the localStorage by using ids from each time-block div
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-  hourTracker();
-});
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    // kick on the hourTracker
+    hourTracker();
 
