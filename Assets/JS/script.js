fb1d915a6a6301a8effb0a0f67739d3388fc57cb
Ronin1702 <kchen@me.com>
2023-05-26 01:24:05 -0400

    A final checkup on all the functionalities

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -13,6 +13,7 @@ $('#currentDay').text(currentDate);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(document).ready(function () {
   //Make a click listener to the saveBtn and get the values stored: 
   $('.saveBtn').on('click', function () {
@@ -26,23 +27,23 @@ $(document).ready(function () {
     // Storing the descriptions and related time-block in the localStorage:
     localStorage.setItem(hour, tasks);
   })
-      // Retrieve items from the localStorage by using ids from each time-block div
-      $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-      $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-      $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-      $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-      $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-      $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-      $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-      $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-      $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  // Retrieve items from the localStorage by using ids from each time-block div
+  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
-// Below I create a listener for the trashBtn and make it retrieve and remove related vals in the localStorage
+  // Below I create a listener for the trashBtn and make it retrieve and remove related vals in the localStorage
   $('.trashBtn').on('click', function () {
     localStorage.removeItem($(this).parent().attr('id'));
     $(this).siblings('.description').val('');
   })
-// Below I created a listener for clearBtn and make it clear out all values and keys in the localStorage
+  // Below I created a listener for clearBtn and make it clear out all values and keys in the localStorage
   $('.clearBtn').on('click', function () {
     localStorage.clear();
     $('.description').val('');
@@ -54,20 +55,20 @@ $(document).ready(function () {
 
     $('.time-block').each(function () {
       var timeBlock = parseInt($(this).attr('id').split('hour-')[1]);
-        // console log to chedck if the varibles are set up alright
-        console.log(timeBlock);
-        console.log(currentHour);
+      // console log to chedck if the varibles are set up alright
+      console.log(timeBlock);
+      console.log(currentHour);
 
       if (timeBlock === currentHour) {
-       
+
         $(this).addClass('present');
 
       } else if (timeBlock > currentHour) {
-       
+
         $(this).addClass('future');
 
       } else {
-        
+
         $(this).addClass('past');
       }
     })
